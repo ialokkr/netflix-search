07f3ef6d3e195ea3b9cede2cc24a71cd21ac92d3
Alok Kumar <Alok.Kumar3@Ltts.com>
2020-12-11 15:11:49 +0530

    initial commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata = [
+    {
+        imgsrc:"https://images-na.ssl-images-amazon.com/images/I/51MmI%2BNl5DL.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname : "DARK",
+        link:"https://dark.netflix.io/en"
+    },
+
+    {
+        imgsrc:"https://www.denofgeek.com/wp-content/uploads/2019/06/stranger-things-season-2-arcade.jpg?fit=620%2C368",
+        title:"A Netflix Original Series" ,
+        sname : "STRANGER THINGS",
+        link:"https://www.netflix.com/title/80057281"
+    },
+
+    {
+        imgsrc:"https://mediaindia.eu/wp-content/uploads/2018/10/SacredGamesstills7.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname: "SACRED GAMES",
+        link:"https://www.netflix.com/title/80115328"
+    },
+
+    {
+        imgsrc:"https://i.dawn.com/large/2020/05/5ec697a9ce9b4.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname : "MONEY HEIST",
+        link:"https://www.netflix.com/in/title/80192098"
+    },
+
+    {
+        imgsrc:"https://cnet1.cbsistatic.com/img/64Tq91O-GU2XQs5kX_CE0wkh5EU=/940x0/2018/07/06/7dcc33c0-0b11-4772-9aed-7b149a7295d8/twd-s9-sdccpanelposter.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname : "THE WALKING DEAD",
+        link:"https://www.netflix.com/in/title/70177057?source=35"
+    }
+]
+
+export default Sdata;
